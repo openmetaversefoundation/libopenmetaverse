b5f1a9ac21d91ed2e3f15c4d0226d000de060a8f
John Hurliman <jhurliman@jhurliman.org>
2006-12-07 01:21:42 +0000

    Added XML deserialization callbacks to ImportCommand to help debugging
    git-svn-id: http://libopenmetaverse.googlecode.com/svn/trunk@689 52acb1d6-8a22-11de-b505-999d5b087335

--- libsecondlife-cs/examples/TestClient/Commands/ImportCommand.cs
+++ libsecondlife-cs/examples/TestClient/Commands/ImportCommand.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using libsecondlife;
 using libsecondlife.Packets;
 using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
 namespace libsecondlife.TestClient
 {
     public class ImportCommand : Command
@@ -17,19 +19,56 @@ namespace libsecondlife.TestClient
         {
             if (args.Length != 1)
                 return "Usage: import inputfile.xml";
+
             string name = args[0];
+            List<PrimObject> prims;
+
             try
             {
-                XmlReader reader = XmlReader.Create(name);
-                List<PrimObject> prims = Helpers.PrimListFromXml(reader);
-                reader.Close();
+                //XmlReader reader = XmlReader.Create(name);
+                //prims = Helpers.PrimListFromXml(reader);
+                //reader.Close();
+
+                XmlSerializer serializer = new XmlSerializer(typeof(List<PrimObject>));
+                XmlDeserializationEvents events = new XmlDeserializationEvents();
+                events.OnUnknownAttribute += new XmlAttributeEventHandler(OnUnknownAttribute);
+                events.OnUnknownElement += new XmlElementEventHandler(OnUnknownElement);
+                events.OnUnknownNode += new XmlNodeEventHandler(OnUnknownNode);
+                events.OnUnreferencedObject += new UnreferencedObjectEventHandler(OnUnreferenced);
+                prims = (List<PrimObject>)serializer.Deserialize(XmlReader.Create(name), events);
+                //return list;
             }
             catch (Exception ex)
             {
                 return "Deserialize failed: " + ex.ToString();
             }
+
             // deserialization done, just need to code to rez and link.
-            return "Deserialized, rez code missing.";
+            return "Deserialized " + prims.Count + " prims, rez code missing.";
+        }
+
+        void OnUnknownAttribute(object obj, XmlAttributeEventArgs args)
+        {
+            // This hasn't happened for me
+            Console.WriteLine("OnUnknownAttribute: " + args.Attr.Name);
+        }
+
+        void OnUnknownElement(object obj, XmlElementEventArgs args)
+        {
+            // Breakpoint here and look at the args class
+            Console.WriteLine(args.Element.Name);
+        }
+
+        void OnUnknownNode(object obj, XmlNodeEventArgs args)
+        {
+            // Breakpoint here and look at the args class
+            Console.WriteLine(args.Name);
+        }
+
+        void OnUnreferenced(object obj, UnreferencedObjectEventArgs args)
+        {
+            // This hasn't happened for me
+            Console.WriteLine("OnUnreferenced: " + args.UnreferencedObject.ToString());
         }
     }
 }
