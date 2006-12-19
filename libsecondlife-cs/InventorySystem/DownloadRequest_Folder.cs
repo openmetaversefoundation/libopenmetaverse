f369c1923a0543131522177fee098ec167c26685
Michael Cortez <mcortez@gmail.com>
2006-12-19 23:15:29 +0000

    Needed for that last commit...
    git-svn-id: http://libopenmetaverse.googlecode.com/svn/trunk@743 52acb1d6-8a22-11de-b505-999d5b087335

---
+++ libsecondlife-cs/InventorySystem/DownloadRequest_Folder.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Threading;
+
+namespace libsecondlife.InventorySystem
+{
+    public class DownloadRequest_Folder
+    {
+            public LLUUID FolderID;
+
+            public int Expected = int.MaxValue;
+            public int Received = 0;
+            public int LastReceivedAtTick = 0;
+
+            public bool FetchFolders = true;
+            public bool FetchItems = true;
+
+            /// <summary>
+            /// Do we want to recursively download this folder?
+            /// </summary>
+            public bool Recurse = true;
+
+            public ManualResetEvent RequestComplete = new ManualResetEvent(false);
+
+            public DownloadRequest_Folder(LLUUID folderID)
+            {
+                FolderID = folderID;
+                LastReceivedAtTick = Environment.TickCount;
+            }
+
+            public DownloadRequest_Folder(LLUUID folderID, bool recurse)
+            {
+                FolderID = folderID;
+                LastReceivedAtTick = Environment.TickCount;
+                Recurse = recurse;
+            }
+
+            public DownloadRequest_Folder(LLUUID folderID, bool fetchFolders, bool fetchItems)
+            {
+                FolderID = folderID;
+                FetchFolders = fetchFolders;
+                FetchItems = fetchItems;
+                LastReceivedAtTick = Environment.TickCount;
+            }
+    }
+}
