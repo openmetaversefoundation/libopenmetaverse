5201b1c37833c62f7e1440b7c7e4dcb6fb246455
John Hurliman <jhurliman@jhurliman.org>
2007-01-22 01:18:38 +0000

    Commented everything in research command out as nant isn't setup to link libsecondlife.Utilities in with TestClient?
    git-svn-id: http://libopenmetaverse.googlecode.com/svn/trunk@868 52acb1d6-8a22-11de-b505-999d5b087335

--- libsecondlife-cs/examples/TestClient/Commands/ResearchCommand.cs
+++ libsecondlife-cs/examples/TestClient/Commands/ResearchCommand.cs
@@ -2,8 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 using libsecondlife;
-using libsecondlife.Utilities.Assets;
-using libsecondlife.Utilities.Appearance;
+//using libsecondlife.Utilities.Assets;
+//using libsecondlife.Utilities.Appearance;
 using libsecondlife.Packets;
 
 namespace libsecondlife.TestClient
@@ -11,62 +11,62 @@ namespace libsecondlife.TestClient
     public class ResearchCommand : Command
     {
         private ManualResetEvent AssetReceived = new ManualResetEvent(false);
-        AssetManager manager;
-        AppearanceManager appearance;
+        //AssetManager manager;
+        //AppearanceManager appearance;
 
         public ResearchCommand(TestClient testClient)
         {
             Name = "research";
             Description = "Does important research for the betterment of mankind";
 
-            manager = new AssetManager(testClient);
-            manager.OnAssetReceived += new AssetManager.AssetReceivedCallback(manager_OnAssetReceived);
-            appearance = new AppearanceManager(testClient, manager);
-            appearance.OnAgentWearables += new AppearanceManager.AgentWearablesCallback(appearance_OnAgentWearables);
+            //manager = new AssetManager(testClient);
+            //manager.OnAssetReceived += new AssetManager.AssetReceivedCallback(manager_OnAssetReceived);
+            //appearance = new AppearanceManager(testClient, manager);
+            //appearance.OnAgentWearables += new AppearanceManager.AgentWearablesCallback(appearance_OnAgentWearables);
         }
 
         public override string Execute(string[] args, LLUUID fromAgentID)
         {
-            Dictionary<WearableType, LLUUID> wearables = new Dictionary<WearableType, LLUUID>();
-            for (int i = 0; i < AppearanceManager.WEARABLE_COUNT; i++)
-            {
-                wearables[(WearableType)i] = LLUUID.Zero;
-            }
-            wearables[WearableType.Shirt] = new LLUUID("4fcf0807-b173-5b4e-90fe-d83d84986b59");
-            appearance.SendAgentWearables(wearables);
+            //Dictionary<WearableType, LLUUID> wearables = new Dictionary<WearableType, LLUUID>();
+            //for (int i = 0; i < AppearanceManager.WEARABLE_COUNT; i++)
+            //{
+            //    wearables[(WearableType)i] = LLUUID.Zero;
+            //}
+            //wearables[WearableType.Shirt] = new LLUUID("4fcf0807-b173-5b4e-90fe-d83d84986b59");
+            //appearance.SendAgentWearables(wearables);
 
-            return "Wearing the payload";
+            //return "Wearing the payload";
 
-            libsecondlife.AssetSystem.AssetWearable asset = new libsecondlife.AssetSystem.AssetWearable(
-                LLUUID.Zero, (sbyte)AssetType.Bodypart, new byte[0]);
-            string exploit = String.Empty;
-            for (int i = 0; i < 3000; i++)
-                exploit += "A";
-            asset.Name = exploit;
-            asset.Type = (sbyte)WearableType.Shirt;
-            LLUUID newasset = Client.Assets.UploadAsset(asset);
+            //libsecondlife.AssetSystem.AssetWearable asset = new libsecondlife.AssetSystem.AssetWearable(
+            //    LLUUID.Zero, (sbyte)AssetType.Bodypart, new byte[0]);
+            //string exploit = String.Empty;
+            //for (int i = 0; i < 3000; i++)
+            //    exploit += "A";
+            //asset.Name = exploit;
+            //asset.Type = (sbyte)WearableType.Shirt;
+            //LLUUID newasset = Client.Assets.UploadAsset(asset);
 
-            return "Created Bodypart " + newasset.ToStringHyphenated();
+            //return "Created Bodypart " + newasset.ToStringHyphenated();
             
 
-            if (args.Length != 1)
-            {
-                appearance.RequestAgentWearables();
+            //if (args.Length != 1)
+            //{
+            //    appearance.RequestAgentWearables();
 
-                return "Appearance fetch complete";
-            }
+            //    return "Appearance fetch complete";
+            //}
 
-            LLUUID id;
-            try
-            {
-                id = new LLUUID(args[0]);
-            }
-            catch (Exception)
-            {
-                return "Need a proper UUID";
-            }
+            //LLUUID id;
+            //try
+            //{
+            //    id = new LLUUID(args[0]);
+            //}
+            //catch (Exception)
+            //{
+            //    return "Need a proper UUID";
+            //}
 
-            manager.RequestAsset(id, AssetType.Bodypart, ChannelType.Asset, SourceType.Asset, 125000.0f);
+            //manager.RequestAsset(id, AssetType.Bodypart, ChannelType.Asset, SourceType.Asset, 125000.0f);
 
             //Random random = new Random();
             //byte r, g, b;
@@ -99,28 +99,28 @@ namespace libsecondlife.TestClient
             return "Research complete";
         }
 
-        void appearance_OnAgentWearables(Dictionary<WearableType, KeyValuePair<LLUUID, LLUUID>> wearables)
-        {
-            foreach (KeyValuePair<WearableType, KeyValuePair<LLUUID, LLUUID>> wearable in wearables)
-            {
-                Console.WriteLine("Received a WearableType " + wearable.Key.ToString() + ", AssetID: " +
-                    wearable.Value.Key.ToStringHyphenated());
-            }
-        }
+        //void appearance_OnAgentWearables(Dictionary<WearableType, KeyValuePair<LLUUID, LLUUID>> wearables)
+        //{
+        //    foreach (KeyValuePair<WearableType, KeyValuePair<LLUUID, LLUUID>> wearable in wearables)
+        //    {
+        //        Console.WriteLine("Received a WearableType " + wearable.Key.ToString() + ", AssetID: " +
+        //            wearable.Value.Key.ToStringHyphenated());
+        //    }
+        //}
 
-        void manager_OnAssetReceived(AssetTransfer asset)
-        {
-            if (asset.Success)
-            {
-                Console.WriteLine("Downloaded " + asset.ID.ToStringHyphenated() + Environment.NewLine + 
-                    Helpers.FieldToUTF8String(asset.AssetData));
-            }
-            else
-            {
-                Console.WriteLine("Failed to retrieve asset " + asset.ID.ToStringHyphenated(), asset.Transferred +
-                    " bytes were transferred");
-            }
-        }
+        //void manager_OnAssetReceived(AssetTransfer asset)
+        //{
+        //    if (asset.Success)
+        //    {
+        //        Console.WriteLine("Downloaded " + asset.ID.ToStringHyphenated() + Environment.NewLine + 
+        //            Helpers.FieldToUTF8String(asset.AssetData));
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Failed to retrieve asset " + asset.ID.ToStringHyphenated(), asset.Transferred +
+        //            " bytes were transferred");
+        //    }
+        //}
 
         //private void FetchInventoryReplyHandler(Packet packet, Simulator simulator)
         //{
