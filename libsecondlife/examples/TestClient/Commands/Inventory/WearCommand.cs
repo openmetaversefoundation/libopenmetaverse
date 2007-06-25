8070059ef9a1bf65934d2aaabf8ccb180d82b54c
jef <example@example.com>
2007-06-25 01:30:47 +0000

    Oops redux.
    git-svn-id: http://libopenmetaverse.googlecode.com/svn/trunk@1240 52acb1d6-8a22-11de-b505-999d5b087335

--- libsecondlife/examples/TestClient/Commands/Inventory/WearCommand.cs
+++ libsecondlife/examples/TestClient/Commands/Inventory/WearCommand.cs
@@ -14,9 +14,8 @@ namespace libsecondlife.TestClient
 {
     public class WearCommand : Command
     {
-		public WearCommand(TestClient testClient)
+		    public WearCommand(TestClient testClient)
         {
-            Client = testClient;
             Name = "wear";
             Description = "Wear an outfit folder from inventory. Usage: wear [outfit name]";
         }
@@ -24,16 +23,9 @@ namespace libsecondlife.TestClient
         public override string Execute(string[] args, LLUUID fromAgentID)
         {
             string target = String.Empty;
-            bool oldcode = false;
-
 
             for (int ct = 0; ct < args.Length; ct++)
-            {
-                if (args[ct] == "oldcode")
-                    oldcode = true;
-                else
-                    target = target + args[ct] + " ";
-            }
+                target = target + args[ct] + " ";
             
             target = target.TrimEnd();
 
@@ -41,13 +33,7 @@ namespace libsecondlife.TestClient
             
             if (folder != null)
             {
-                //libsecondlife.Utilities.Appearance.AppearanceManager am = new libsecondlife.Utilities.Appearance.AppearanceManager(Client, new Utilities.Assets.AssetManager(Client));
-                
-                if (oldcode)
-                    Client.Appearance.WearOutfit(folder);
-                else
-                    Client.NewAppearanceManager.WearOutfit(folder);
-
+                Client.Appearance.WearOutfit(folder);
                 return "Outfit " + target + " worn.";
             }
 
