ae1861d0f0b278e7f2c717a04022b9d9a7411540
Jim Radford <jradford@npl.com>
2008-02-28 22:14:46 +0000

    Applies patch from A_Biondi, adds Enumerator for TextureAnimMode. Mantis#140
    git-svn-id: http://libopenmetaverse.googlecode.com/svn/trunk@1652 52acb1d6-8a22-11de-b505-999d5b087335

--- libsecondlife/Prims.cs
+++ libsecondlife/Prims.cs
@@ -53,6 +53,30 @@ namespace libsecondlife
         /// <summary>
         /// 
         /// </summary>
+        [Flags]
+        public enum TextureAnimMode : uint
+        {
+            /// <summary>Disable texture animation</summary>
+            ANIM_OFF = 0x00,
+            /// <summary>Enable texture animation</summary>
+            ANIM_ON = 0x01,
+            /// <summary>Loop when animating textures</summary>
+            LOOP = 0x02,
+            /// <summary>Animate in reverse direction</summary>
+            REVERSE = 0x04,
+            /// <summary>Animate forward then reverse</summary>
+            PING_PONG = 0x08,
+            /// <summary>Slide texture smoothly instead of frame-stepping</summary>
+            SMOOTH = 0x10,
+            /// <summary>Rotate texture instead of using frames</summary>
+            ROTATE = 0x20,
+            /// <summary>Scale texture instead of using frames</summary>
+            SCALE = 0x40,
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public enum JointType : byte
         {
             /// <summary></summary>
@@ -121,7 +145,7 @@ namespace libsecondlife
         public struct TextureAnimation
         {
             /// <summary></summary>
-            public uint Flags;
+            public TextureAnimMode Flags;
             /// <summary></summary>
             public uint Face;
             /// <summary></summary>
@@ -144,7 +168,7 @@ namespace libsecondlife
             {
                 if (data.Length >= 16)
                 {
-                    Flags = (uint)data[pos++];
+                    Flags = (TextureAnimMode)((uint)data[pos++]);
                     Face = (uint)data[pos++];
                     SizeX = (uint)data[pos++];
                     SizeY = (uint)data[pos++];
