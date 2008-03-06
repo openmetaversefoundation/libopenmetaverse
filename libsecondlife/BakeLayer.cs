079af6ccc4230e018e3b4b627245eaee02fcdc9d
Jim Radford <jradford@npl.com>
2008-03-06 06:04:22 +0000

    New Feature implements simple avatar texture baking courtesy of dahlia, mantis#153
    git-svn-id: http://libopenmetaverse.googlecode.com/svn/trunk@1657 52acb1d6-8a22-11de-b505-999d5b087335

--- libsecondlife/BakeLayer.cs
+++ libsecondlife/BakeLayer.cs
@@ -28,6 +28,8 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace libsecondlife.Baking
 {
@@ -42,7 +44,7 @@ namespace libsecondlife.Baking
 
         /// <summary>Appearance parameters the drive the baking process</summary>
         protected Dictionary<int, float> ParamValues;
-        
+
         /// <summary>Wearable textures</summary>
         protected Dictionary<AppearanceManager.TextureIndex, AssetTexture> Textures = new Dictionary<AppearanceManager.TextureIndex, AssetTexture>();
         protected int TextureCount;
@@ -84,9 +86,11 @@ namespace libsecondlife.Baking
             ParamValues = paramValues;
 
             if (textureCount == 0)
+            {
                 Bake();
+            }
         }
-        
+
         /// <summary>
         /// Adds an image to this baking texture and potentially processes it, or
         /// stores it for processing later
@@ -100,14 +104,24 @@ namespace libsecondlife.Baking
         {
             lock (Textures)
             {
-                texture.Decode();
-                Textures.Add(index, texture);
-                Client.DebugLog("Adding texture " + index.ToString() + " to bake " + BakeType.ToString());
+                try 
+                {
+
+                    texture.Decode();
+                    Textures.Add(index, texture);
+                    Client.DebugLog("Adding texture " + index.ToString() + " ID: " + texture.AssetID.ToString() + " to bake " + BakeType.ToString());
+                }
+                catch ( Exception e )
+                {
+                    Client.DebugLog( "caught exception while trying add texture: " + e.Message.ToString());
+                }
+
             }
 
             if (Textures.Count == TextureCount)
             {
                 Bake();
+                
                 return true;
             }
             else
@@ -128,7 +142,140 @@ namespace libsecondlife.Baking
                 return false;
         }
 
+
         protected void Bake()
+        {
+            BakedTexture = new AssetTexture(new Image(BakeWidth, BakeHeight, ImageChannels.Color | ImageChannels.Alpha | ImageChannels.Bump));
+
+            if (BakeType == AppearanceManager.BakeType.Eyes)
+            {
+                if (!DrawLayer(AppearanceManager.TextureIndex.EyesIris))
+                    oldBake();
+            }
+            else if (BakeType == AppearanceManager.BakeType.Head)
+            {
+                if (!DrawLayer(AppearanceManager.TextureIndex.HeadBodypaint))
+                    oldBake();
+            }
+            else if (BakeType == AppearanceManager.BakeType.Skirt)
+            {
+                if (!DrawLayer(AppearanceManager.TextureIndex.Skirt))
+                    oldBake();
+            }
+            else if (BakeType == AppearanceManager.BakeType.UpperBody)
+            {
+                if (!DrawLayer(AppearanceManager.TextureIndex.UpperBodypaint))
+                    oldBake();
+                DrawLayer(AppearanceManager.TextureIndex.UpperUndershirt);
+                DrawLayer(AppearanceManager.TextureIndex.UpperGloves);
+                DrawLayer(AppearanceManager.TextureIndex.UpperShirt);
+                DrawLayer(AppearanceManager.TextureIndex.UpperJacket);
+            }
+            else if (BakeType == AppearanceManager.BakeType.LowerBody)
+            {
+                if (!DrawLayer(AppearanceManager.TextureIndex.LowerBodypaint))
+                    oldBake();
+                DrawLayer(AppearanceManager.TextureIndex.LowerUnderpants);
+                DrawLayer(AppearanceManager.TextureIndex.LowerSocks);
+                DrawLayer(AppearanceManager.TextureIndex.LowerShoes);
+                DrawLayer(AppearanceManager.TextureIndex.LowerPants);
+                DrawLayer(AppearanceManager.TextureIndex.LowerJacket);
+            }
+
+            BakedTexture.Encode();
+
+        }
+
+        private bool DrawLayer(AppearanceManager.TextureIndex textureIndex)
+        {
+            int i = 0;
+
+            AssetTexture texture = new AssetTexture();
+
+            if ( ! Textures.TryGetValue(textureIndex, out texture))
+                return false;
+
+            Client.DebugLog("DrawLayer(): baking layer " + textureIndex.ToString());
+
+            Image source = texture.Image;
+
+            source.ResizeNearestNeighbor(BakeWidth, BakeHeight);
+
+            if (textureIndex == AppearanceManager.TextureIndex.HeadBodypaint
+                || textureIndex == AppearanceManager.TextureIndex.UpperBodypaint
+                || textureIndex == AppearanceManager.TextureIndex.LowerBodypaint
+                || textureIndex == AppearanceManager.TextureIndex.Skirt
+                || textureIndex == AppearanceManager.TextureIndex.EyesIris)
+            { // initial layer, just copy onto baked layer
+
+                for (int y = 0; y < BakeHeight; y++)
+                {
+                    for (int x = 0; x < BakeWidth; x++)
+                    {
+                        if ((source.Channels & ImageChannels.Alpha) != 0)
+                        {
+                            if (source.Alpha[i] != 0)
+                            {
+                                BakedTexture.Image.Red[i] = source.Red[i];
+                                BakedTexture.Image.Green[i] = source.Green[i];
+                                BakedTexture.Image.Blue[i] = source.Blue[i];
+
+                                BakedTexture.Image.Alpha[i] = source.Alpha[i];
+                                BakedTexture.Image.Bump[i] = 0;
+                            }
+                        }
+                        else
+                        {
+                            BakedTexture.Image.Red[i] = source.Red[i];
+                            BakedTexture.Image.Green[i] = source.Green[i];
+                            BakedTexture.Image.Blue[i] = source.Blue[i];
+                            BakedTexture.Image.Alpha[i] = 255;
+
+                            BakedTexture.Image.Bump[i] = 0;
+                        }
+
+                        ++i;
+                    }
+                }
+            }
+            else // not skin layer, so composite with alpha blending
+            {
+
+                for (int y = 0; y < BakeHeight; y++)
+                {
+                    for (int x = 0; x < BakeWidth; x++)
+                    {
+
+                        float alpha = (float)source.Alpha[i];
+                        alpha /= 255.0f;
+                        float red = (float)source.Red[i];
+                        float green = (float)source.Green[i];
+                        float blue = (float)source.Blue[i];
+                        red /= 255.0f;
+                        green /= 255.0f;
+                        blue /= 255.0f;
+
+                        float currRed = (float)BakedTexture.Image.Red[i];
+                        float currGreen = (float)BakedTexture.Image.Green[i];
+                        float currBlue = (float)BakedTexture.Image.Blue[i];
+
+                        currRed /= 255.0F;
+                        currGreen /= 255.0F;
+                        currBlue /= 255.0F;
+
+                        BakedTexture.Image.Red[i] = (byte)(255.0f * ((currRed * (1.0 - alpha)) + (red * alpha)));
+
+                        BakedTexture.Image.Green[i] = (byte)(255.0f * ((currGreen * (1.0 - alpha)) + (green * alpha)));
+                        BakedTexture.Image.Blue[i] = (byte)(255.0f * ((currBlue * (1.0 - alpha)) + (blue * alpha)));
+
+                        i++;
+                    }
+                }
+            }
+            return true;
+        }
+
+        protected void oldBake()
         {
             Client.DebugLog("Baking " + BakeType.ToString());
             BakedTexture = new AssetTexture(new Image(BakeWidth, BakeHeight, ImageChannels.Color | ImageChannels.Alpha | ImageChannels.Bump));
