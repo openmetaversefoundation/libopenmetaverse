9cbc2d010c9a67330fdf8d5c02abf0db09bb48aa
Jim Radford <jradford@npl.com>
2008-06-25 17:47:59 +0000

    LIBOMV-81 Adds methods for playing sound files to SoundManager
    git-svn-id: http://libopenmetaverse.googlecode.com/svn/trunk@1899 52acb1d6-8a22-11de-b505-999d5b087335

--- libsecondlife/SoundManager.cs
+++ libsecondlife/SoundManager.cs
@@ -54,6 +54,72 @@ namespace libsecondlife
             Client.Network.RegisterCallback(PacketType.SoundTrigger, new NetworkManager.PacketCallback(SoundTriggerHandler));
         }
 
+        #region public methods
+
+        /// <summary>
+        /// Plays a sound in the current region at full volume from avatar position
+        /// </summary>
+        /// <param name="soundID">UUID of the sound to be played</param>
+        public void SoundTrigger(LLUUID soundID)
+        {
+            SoundTrigger(soundID, Client.Self.SimPosition, 1.0f);
+        }
+
+        /// <summary>
+        /// Plays a sound in the current region at full volume
+        /// </summary>
+        /// <param name="soundID">UUID of the sound to be played.</param>
+        /// <param name="position">position for the sound to be played at. Normally the avatar.</param>
+        public void SoundTrigger(LLUUID soundID, LLVector3 position)
+        {
+            SoundTrigger(soundID, Client.Self.SimPosition, 1.0f);
+        }
+
+        /// <summary>
+        /// Plays a sound in the current region
+        /// </summary>
+        /// <param name="soundID">UUID of the sound to be played.</param>
+        /// <param name="position">position for the sound to be played at. Normally the avatar.</param>
+        /// <param name="gain">volume of the sound, from 0.0 to 1.0</param>
+        public void SoundTrigger(LLUUID soundID, LLVector3 position, float gain)
+        {
+            SoundTrigger(soundID, Client.Network.CurrentSim.Handle, position, 1.0f);
+        }
+        /// <summary>
+        /// Plays a sound in the specified sim
+        /// </summary>
+        /// <param name="soundID">UUID of the sound to be played.</param>
+        /// <param name="sim">UUID of the sound to be played.</param>
+        /// <param name="position">position for the sound to be played at. Normally the avatar.</param>
+        /// <param name="gain">volume of the sound, from 0.0 to 1.0</param>
+        public void SoundTrigger(LLUUID soundID, Simulator sim, LLVector3 position, float gain)
+        {
+            SoundTrigger(soundID, sim.Handle, position, 1.0f);
+        }
+
+        /// <summary>
+        /// Plays a sound
+        /// </summary>
+        /// <param name="soundID">UUID of the sound to be played.</param>
+        /// <param name="handle">handle id for the sim to be played in.</param>
+        /// <param name="position">position for the sound to be played at. Normally the avatar.</param>
+        /// <param name="gain">volume of the sound, from 0.0 to 1.0</param>
+        public void SoundTrigger(LLUUID soundID, ulong handle, LLVector3 position, float gain)
+        {
+            SoundTriggerPacket soundtrigger = new SoundTriggerPacket();
+            soundtrigger.SoundData = new SoundTriggerPacket.SoundDataBlock();
+            soundtrigger.SoundData.SoundID = soundID;
+            soundtrigger.SoundData.ObjectID = LLUUID.Zero;
+            soundtrigger.SoundData.OwnerID = LLUUID.Zero;
+            soundtrigger.SoundData.ParentID = LLUUID.Zero;
+            soundtrigger.SoundData.Handle = handle;
+            soundtrigger.SoundData.Position = position;
+            soundtrigger.SoundData.Gain = gain;
+            Client.Network.SendPacket(soundtrigger);
+        }
+
+        #endregion
+        #region Packet Handlers
         protected void AttachedSoundHandler(Packet packet, Simulator simulator)
         {
             AttachedSoundPacket sound = (AttachedSoundPacket)packet;
@@ -110,5 +176,6 @@ namespace libsecondlife
                 catch (Exception e) { Logger.Log(e.Message, Helpers.LogLevel.Error, Client, e); }
             }
         }
+        #endregion
     }
 }
