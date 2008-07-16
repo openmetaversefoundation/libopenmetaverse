f8d9fb21bbcb0ca8b42a5b5d3d996e2bab0d86a2
Jim Radford <jradford@npl.com>
2008-07-16 02:37:23 +0000

    TC-42 Fixes previously disabled TestClient DeleteFolderCommand
    git-svn-id: http://libopenmetaverse.googlecode.com/svn/trunk@1914 52acb1d6-8a22-11de-b505-999d5b087335

--- libsecondlife/examples/TestClient/Commands/Inventory/DeleteFolderCommand.cs
+++ libsecondlife/examples/TestClient/Commands/Inventory/DeleteFolderCommand.cs
@@ -9,32 +9,43 @@ using libsecondlife;
 
 namespace libsecondlife.TestClient
 {
+    /// <summary>
+    /// Inventory Example, Moves a folder to the Trash folder
+    /// </summary>
     public class DeleteFolderCommand : Command
     {
 		public DeleteFolderCommand(TestClient testClient)
         {
             Name = "deleteFolder";
-            Description = "Deletes a folder from inventory.";
+            Description = "Moves a folder to the Trash Folder";
         }
 
         public override string Execute(string[] args, LLUUID fromAgentID)
         {
-            return "Broken until someone fixes me";
+            // parse the command line
+            string target = String.Empty;
+            for (int ct = 0; ct < args.Length; ct++)
+                target = target + args[ct] + " ";
+            target = target.TrimEnd();
 
-            //string target = String.Empty;
-            //for (int ct = 0; ct < args.Length; ct++)
-            //    target = target + args[ct] + " ";
-            //target = target.TrimEnd();
-
-            //Client.Inventory.DownloadInventory();
-            //InventoryFolder folder = Client.Inventory.getFolder(target);
-            //if (folder != null)
-            //{
-            //    folder.Delete();
-            //    return "Folder " + target + " deleted.";
-            //}
-
-            //return "Unable to find: " + target;
+            // initialize results list
+            List<InventoryBase> found = new List<InventoryBase>();
+            try
+            {
+                // find the folder
+                found = Client.Inventory.LocalFind(Client.Inventory.Store.RootFolder.UUID, target.Split('/'), 0, true);
+                if (found.Count.Equals(1))
+                {
+                    // move the folder to the trash folder
+                    Client.Inventory.MoveFolder(found[0].UUID, Client.Inventory.FindFolderForType(AssetType.TrashFolder));
+                    return String.Format("Moved folder {0} to Trash", found[0].Name);
+                }
+            }
+            catch (InvalidOutfitException ex)
+            {
+                return "Folder Not Found: (" + ex.Message + ")";
+            }
+            return string.Empty;
 		}
 	}
 }
