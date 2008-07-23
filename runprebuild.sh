ac99354c14e427cfbed6482c356c14c4417e530f
Jim Radford <jradford@npl.com>
2008-07-23 22:25:59 +0000

    Fixed prebuild script to return exit code from nant during build of specified
    git-svn-id: http://libopenmetaverse.googlecode.com/svn/trunk@1981 52acb1d6-8a22-11de-b505-999d5b087335

--- runprebuild.sh
+++ runprebuild.sh
@@ -7,10 +7,12 @@ mono bin/Prebuild.exe /target vs2005
 if [ x$1 == xnant ]
 then
         nant -buildfile:OpenMetaverse.build
+	exit $?
 fi
 
 if [ x$1 == xprimrender ]
 then
 	nant -buildfile:OpenMetaverse.Rendering.GPL.build
+	exit $?
 fi
 
