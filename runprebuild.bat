36399c9ad67c1f61aba9ee836794bde407d6ae89
Jim Radford <jradford@npl.com>
2008-07-23 08:28:18 +0000

    Update Prebuild configuration to again build nightly API Documentation * Removed unnecessary files from old Documentation Project * Updated Documentation Generator to use new library name * Still need to update the binary installer generator scripts
    git-svn-id: http://libopenmetaverse.googlecode.com/svn/trunk@1973 52acb1d6-8a22-11de-b505-999d5b087335

--- runprebuild.bat
+++ runprebuild.bat
@@ -2,3 +2,6 @@ bin\Prebuild.exe /target nant
 bin\Prebuild.exe /target vs2005
 
 echo C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727\msbuild OpenMetaverse.sln > compile.bat
+echo C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727\msbuild /p:Configuration=Release OpenMetaverse.sln > builddocs.bat
+echo SandCastleBuilderConsole.exe docs\OpenMetaverse-docs.shfb >> builddocs.bat
+echo 7za.exe a -tzip docs\documentation.zip docs\trunk >> builddocs.bat
