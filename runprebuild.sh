88b0cd6f9fde3fa9a72339f4bc19339415175485
Jim Radford <jradford@npl.com>
2008-07-23 22:12:26 +0000

    Fixes prebuild to support building multiple targets using nant, pass 'nant' to build the openmetaverse library, or 'render' to build the libprimrender library
    git-svn-id: http://libopenmetaverse.googlecode.com/svn/trunk@1980 52acb1d6-8a22-11de-b505-999d5b087335

--- runprebuild.sh
+++ runprebuild.sh
@@ -6,6 +6,11 @@ mono bin/Prebuild.exe /target vs2005
 
 if [ x$1 == xnant ]
 then
-        nant
+        nant -buildfile:OpenMetaverse.build
+fi
+
+if [ x$1 == xprimrender ]
+then
+	nant -buildfile:OpenMetaverse.Rendering.GPL.build
 fi
 
