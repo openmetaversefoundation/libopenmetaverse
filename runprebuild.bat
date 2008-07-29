f042f47d6aa9cd57244e9a38487152fd20f4bb21
Jim Radford <jradford@npl.com>
2008-07-29 22:16:17 +0000

    Adds support to build system for running unit tests on buildserver
    git-svn-id: http://libopenmetaverse.googlecode.com/svn/trunk@2022 52acb1d6-8a22-11de-b505-999d5b087335

--- runprebuild.bat
+++ runprebuild.bat
@@ -3,10 +3,14 @@ bin\Prebuild.exe /target nant
 bin\Prebuild.exe /target vs2005
 
 echo C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727\msbuild OpenMetaverse.sln > compile.bat
+
 echo C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727\msbuild /p:Configuration=Release OpenMetaverse.sln > builddocs.bat
 echo SandCastleBuilderConsole.exe docs\OpenMetaverse-docs.shfb >> builddocs.bat
 echo 7za.exe a -tzip docs\documentation.zip docs\trunk >> builddocs.bat
 
+if(%2)==(runtests) echo "c:\Program Files\NUnit 2.4.7\bin\nunit-console.exe" bin\OpenMetaverse.Tests.dll > runtests.bat
+
 if(%1)==(msbuild) compile.bat
 if(%1)==(docs) builddocs.bat
 if(%1)==(nant) nant
+if(%2)==(runtests) runtests.bat
--- runprebuild2008.bat
+++ runprebuild2008.bat
@@ -8,6 +8,9 @@ echo C:\WINDOWS\Microsoft.NET\Framework\v3.5\msbuild /p:Configuration=Release Op
 echo SandCastleBuilderConsole.exe docs\OpenMetaverse-docs.shfb >> builddocs.bat
 echo 7za.exe a -tzip docs\documentation.zip docs\trunk >> builddocs.bat
 
+if(%2)==(runtests) echo "c:\Program Files\NUnit 2.4.7\bin\nunit-console.exe" bin\OpenMetaverse.Tests.dll > runtests.bat
+
 if(%1)==(msbuild) compile.bat
 if(%1)==(docs) builddocs.bat
 if(%1)==(nant) nant
+if(%2)==(runtests) runtests.bat
