26a71d840690ea22ca642fb7edb6b44bdf85a34e
Latif Khalifa <latifer@streamgrid.net>
2008-08-21 19:52:48 +0000

    LIBOMV-360: update mapgen.bat for the new file layout
    git-svn-id: http://libopenmetaverse.googlecode.com/svn/trunk@2143 52acb1d6-8a22-11de-b505-999d5b087335

--- mapgen.bat
+++ mapgen.bat
@@ -1,1 +1,1 @@
-"bin/mapgenerator.exe" data/message_template.msg libsecondlife/mapgenerator/template.cs libsecondlife/mapgenerator/unusedpackets.txt libsecondlife/_Packets_.cs
+"bin/mapgenerator.exe" data/message_template.msg Programs/mapgenerator/template.cs Programs/mapgenerator/unusedpackets.txt OpenMetaverse/_Packets_.cs
