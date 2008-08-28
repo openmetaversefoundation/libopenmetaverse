274ca8a290bade1c8415bf35086b609761906426
John Hurliman <jhurliman@jhurliman.org>
2008-08-28 22:04:25 +0000

    Adds a simple DoubleDictionary class to Simian that allows two keys to index the same value, and internally handles locking mechanics
    git-svn-id: http://libopenmetaverse.googlecode.com/svn/trunk@2179 52acb1d6-8a22-11de-b505-999d5b087335

---
+++ Programs/Simian/DoubleDictionary.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+
+namespace Simian
+{
+    public class DoubleDictionary<TKey1, TKey2, TValue>
+    {
+        Dictionary<TKey1, TValue> Dictionary1;
+        Dictionary<TKey2, TValue> Dictionary2;
+        object syncObject = new object();
+
+        public DoubleDictionary()
+        {
+            Dictionary1 = new Dictionary<TKey1,TValue>();
+            Dictionary2 = new Dictionary<TKey2,TValue>();
+        }
+
+        public DoubleDictionary(int capacity)
+        {
+            Dictionary1 = new Dictionary<TKey1, TValue>(capacity);
+            Dictionary2 = new Dictionary<TKey2, TValue>(capacity);
+        }
+
+        public void Add(TKey1 key1, TKey2 key2, TValue value)
+        {
+            lock (syncObject)
+            {
+                Dictionary1.Add(key1, value);
+                Dictionary2.Add(key2, value);
+            }
+        }
+
+        public bool Remove(TKey1 key1, TKey2 key2)
+        {
+            lock (syncObject)
+            {
+                Dictionary1.Remove(key1);
+                return Dictionary2.Remove(key2);
+            }
+        }
+
+        public bool TryGetValue(TKey1 key, out TValue value)
+        {
+            return Dictionary1.TryGetValue(key, out value);
+        }
+
+        public bool TryGetValue(TKey2 key, out TValue value)
+        {
+            return Dictionary2.TryGetValue(key, out value);
+        }
+
+        public void ForEach(Action<TValue> action)
+        {
+            lock (syncObject)
+            {
+                foreach (TValue value in Dictionary1.Values)
+                    action(value);
+            }
+        }
+    }
+}
