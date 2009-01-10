194377e637c25d8b9de6fe31696f08cdfd1def75
John Hurliman <jhurliman@jhurliman.org>
2009-01-10 00:21:06 +0000

    Specify #!/bin/bash instead of #!/bin/sh to fix errors when running in a non-bash terminal
    git-svn-id: http://libopenmetaverse.googlecode.com/svn/libopenmetaverse/trunk@2412 52acb1d6-8a22-11de-b505-999d5b087335

--- runprebuild.sh
+++ runprebuild.sh
@@ -1,18 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
 mono bin/Prebuild.exe /target nant
 mono bin/Prebuild.exe /target monodev
 mono bin/Prebuild.exe /target vs2005
 
-if [ x$1 == xnant ]
-then
-        nant -buildfile:OpenMetaverse.build
-	exit $?
+if [ x$1 == xnant ]; then
+    nant -buildfile:OpenMetaverse.build
+    exit $?
 fi
 
-if [ x$1 == xprimrender ]
-then
-	nant -buildfile:OpenMetaverse.Rendering.GPL.build
-	exit $?
+if [ x$1 == xprimrender ]; then
+    nant -buildfile:OpenMetaverse.Rendering.GPL.build
+    exit $?
 fi
-
