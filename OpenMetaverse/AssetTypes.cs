b418d85e005956a34500ad7f459339bc74d20076
Jim Radford <jradford@npl.com>
2009-03-19 01:25:28 +0000

    Adds XML documentation to the Asset* Classes Removes ObjectType Enum which was renamed to InventoryItemType and is now contained in InventoryManager
    git-svn-id: http://libopenmetaverse.googlecode.com/svn/libopenmetaverse/trunk@2505 52acb1d6-8a22-11de-b505-999d5b087335

--- OpenMetaverse/AssetTypes.cs
+++ OpenMetaverse/AssetTypes.cs
@@ -68,53 +68,41 @@ namespace OpenMetaverse
     };
 
     /// <summary>
-    /// Each inventory AssetType will have its own set of flags, these are the known flags for AssetType=Object
+    /// Base class for all Asset types
     /// </summary>
-    [Flags]
-    public enum ObjectType : uint
-    {
-        None = 0,
-        /// <summary>
-        /// A Landmark that has not been previously visited shows up as a dark red pushpin, one that has been
-        /// visited shows up as a light red pushpin
-        /// </summary>
-        VisitedLandmark = 1,
-        /// <summary>If set, indicates rezzed object will have more restrictive permissions masks;
-        /// Which masks will be affected are below</summary>
-        RestrictNextOwner = 0x100,
-        /// <summary>If set, and <c>RestrictNextOwner</c> bit is set indicates BaseMask will be overwritten on Rez</summary>
-        OverwriteBase = 0x010000,
-        /// <summary>If set, and <c>RestrictNextOwner</c> bit is set indicates OwnerMask will be overwritten on Rez</summary>
-        OverwriteOwner = 0x020000,
-        /// <summary>If set, and <c>RestrictNextOwner</c> bit is set indicates GroupMask will be overwritten on Rez</summary>
-        OverwriteGroup = 0x040000,
-        /// <summary>If set, and <c>RestrictNextOwner</c> bit is set indicates EveryoneMask will be overwritten on Rez</summary>
-        OverwriteEveryone = 0x080000,
-        /// <summary>If set, and <c>RestrictNextOwner</c> bit is set indicates NextOwnerMask will be overwritten on Rez</summary>
-        OverwriteNextOwner = 0x100000,
-        /// <summary>If set, indicates item is multiple items coalesced into a single item</summary>
-        MultipleObjects = 0x200000
-    }
-
     public abstract class Asset
     {
+        /// <summary>A byte array containing the raw asset data</summary>
         public byte[] AssetData;
+        /// <summary>True if the asset it only stored on the server temporarily</summary>
         public bool Temporary;
-
+        /// <summary>A unique ID</summary>
         private UUID _AssetID;
+        /// <summary>The assets unique ID</summary>
         public UUID AssetID
         {
             get { return _AssetID; }
             internal set { _AssetID = value; }
         }
 
+        /// <summary>
+        /// The "type" of asset, Notecard, Animation, etc
+        /// </summary>
         public abstract AssetType AssetType
         {
             get;
         }
 
+        /// <summary>
+        /// Construct a new Asset object
+        /// </summary>
         public Asset() { }
 
+        /// <summary>
+        /// Construct a new Asset object
+        /// </summary>
+        /// <param name="assetID">A unique <see cref="UUID"/> specific to this asset</param>
+        /// <param name="assetData">A byte array containing the raw asset data</param>
         public Asset(UUID assetID, byte[] assetData)
         {
             _AssetID = assetID;
@@ -135,12 +123,22 @@ namespace OpenMetaverse
         public abstract bool Decode();
     }
 
+    /// <summary>
+    /// Represents an Animation
+    /// </summary>
     public class AssetAnimation : Asset
     {
+        /// <summary>Override the base classes AssetType</summary>
         public override AssetType AssetType { get { return AssetType.Animation; } }
 
+        /// <summary>Default Constructor</summary>
         public AssetAnimation() { }
 
+        /// <summary>
+        /// Construct an Asset object of type Animation
+        /// </summary>
+        /// <param name="assetID">A unique <see cref="UUID"/> specific to this asset</param>
+        /// <param name="assetData">A byte array containing the raw asset data</param>
         public AssetAnimation(UUID assetID, byte[] assetData)
             : base(assetID, assetData)
         {
@@ -151,28 +149,45 @@ namespace OpenMetaverse
         public override bool Decode() { return true; }
     }
 
+    /// <summary>
+    /// Represents a string of characters encoded with specific formatting properties
+    /// </summary>
     public class AssetNotecard : Asset
     {
+        /// <summary>Override the base classes AssetType</summary>
         public override AssetType AssetType { get { return AssetType.Notecard; } }
-
+        
+        /// <summary>A text string containing the raw contents of the notecard</summary>
         public string Text = null;
 
+        /// <summary>Construct an Asset of type Notecard</summary>
         public AssetNotecard() { }
-        
+
+        /// <summary>
+        /// Construct an Asset object of type Notecard
+        /// </summary>
+        /// <param name="assetID">A unique <see cref="UUID"/> specific to this asset</param>
+        /// <param name="assetData">A byte array containing the raw asset data</param>
         public AssetNotecard(UUID assetID, byte[] assetData) : base(assetID, assetData) 
         {
             Decode();
         }
         
+        /// <summary>
+        /// Construct an Asset object of type Notecard
+        /// </summary>
+        /// <param name="text">A text string containing the raw contents of the notecard</param>
         public AssetNotecard(string text)
         {
             Text = text;
             Encode();
         }
 
+        /// <summary>
+        /// Encode the raw contents of a string with the specific Linden Text properties
+        /// </summary>
         public override void Encode()
         {
-
             string temp = "Linden text version 2\n{\nLLEmbeddedItems version 1\n{\ncount 0\n}\nText length ";
             temp += Text.Length + "\n";
             temp += Text;
@@ -180,6 +195,10 @@ namespace OpenMetaverse
             AssetData = Utils.StringToBytes(temp);
         }
         
+        /// <summary>
+        /// Decode the raw asset data including the Linden Text properties
+        /// </summary>
+        /// <returns>true if the AssetData was successfully decoded to a string</returns>
         public override bool Decode()
         {
             Text = Utils.BytesToString(AssetData);
@@ -187,26 +206,48 @@ namespace OpenMetaverse
         }
     }
 
+    /// <summary>
+    /// Represents an LSL Text object containing a string of UTF encoded characters
+    /// </summary>
     public class AssetScriptText : Asset
     {
+        /// <summary>Override the base classes AssetType</summary>
         public override AssetType AssetType { get { return AssetType.LSLText; } }
 
+        /// <summary>A string of characters represting the script contents</summary>
         public string Source;
 
+        /// <summary>Initializes a new AssetScriptText object</summary>
         public AssetScriptText() { }
 
+        /// <summary>
+        /// Initializes a new AssetScriptText object with parameters
+        /// </summary>
+        /// <param name="assetID">A unique <see cref="UUID"/> specific to this asset</param>
+        /// <param name="assetData">A byte array containing the raw asset data</param>
         public AssetScriptText(UUID assetID, byte[] assetData) : base(assetID, assetData) { }
         
+        /// <summary>
+        /// Initializes a new AssetScriptText object with parameters
+        /// </summary>
+        /// <param name="source">A string containing the scripts contents</param>
         public AssetScriptText(string source)
         {
             Source = source;
         }
 
+        /// <summary>
+        /// Encode a string containing the scripts contents into byte encoded AssetData
+        /// </summary>
         public override void Encode()
         {
             AssetData = Utils.StringToBytes(Source);
         }
 
+        /// <summary>
+        /// Decode a byte array containing the scripts contents into a string
+        /// </summary>
+        /// <returns>true if decoding is successful</returns>
         public override bool Decode()
         {
             Source = Utils.BytesToString(AssetData);
@@ -214,50 +255,102 @@ namespace OpenMetaverse
         }
     }
 
+    /// <summary>
+    /// Represents an AssetScriptBinary object containing the 
+    /// LSO compiled bytecode of an LSL script
+    /// </summary>
     public class AssetScriptBinary : Asset
     {
+        /// <summary>Override the base classes AssetType</summary>
         public override AssetType AssetType { get { return AssetType.LSLBytecode; } }
 
+        /// <summary>Initializes a new instance of an AssetScriptBinary object</summary>
         public AssetScriptBinary() { }
 
+        /// <summary>Initializes a new instance of an AssetScriptBinary object with parameters</summary>
+        /// <param name="assetID">A unique <see cref="UUID"/> specific to this asset</param>
+        /// <param name="assetData">A byte array containing the raw asset data</param>
         public AssetScriptBinary(UUID assetID, byte[] assetData)
             : base(assetID, assetData)
         {
             AssetData = assetData;
         }
 
+        /// <summary>
+        /// TODO: Encodes a scripts contents into a LSO Bytecode file
+        /// </summary>
         public override void Encode() { }
+
+        /// <summary>
+        /// TODO: Decode LSO Bytecode into a string
+        /// </summary>
+        /// <returns>true</returns>
         public override bool Decode() { return true; }
     }
 
+    /// <summary>
+    /// Represents a Sound Asset
+    /// </summary>
     public class AssetSound : Asset
     {
+        /// <summary>Override the base classes AssetType</summary>
         public override AssetType AssetType { get { return AssetType.Sound; } }
 
+        /// <summary>Initializes a new instance of an AssetSound object</summary>
         public AssetSound() { }
 
+        /// <summary>Initializes a new instance of an AssetSound object with parameters</summary>
+        /// <param name="assetID">A unique <see cref="UUID"/> specific to this asset</param>
+        /// <param name="assetData">A byte array containing the raw asset data</param>
         public AssetSound(UUID assetID, byte[] assetData)
             : base(assetID, assetData)
         {
             AssetData = assetData;
         }
 
+        /// <summary>
+        /// TODO: Encodes a sound file
+        /// </summary>
         public override void Encode() { }
+
+        /// <summary>
+        /// TODO: Decode a sound file
+        /// </summary>
+        /// <returns>true</returns>
         public override bool Decode() { return true; }
     }
 
+    /// <summary>
+    /// Represents a texture
+    /// </summary>
     public class AssetTexture : Asset
     {
+        /// <summary>Override the base classes AssetType</summary>
         public override AssetType AssetType { get { return AssetType.Texture; } }
 
+        /// <summary>A <seealso cref="ManagedImage"/> object containing image data</summary>
         public ManagedImage Image;
+
+        /// <summary></summary>
         public OpenJPEG.J2KLayerInfo[] LayerInfo;
+
+        /// <summary></summary>
         public int Components;
-        
+
+        /// <summary>Initializes a new instance of an AssetTexture object</summary>
         public AssetTexture() { }
 
+        /// <summary>
+        /// Initializes a new instance of an AssetTexture object
+        /// </summary>
+        /// <param name="assetID">A unique <see cref="UUID"/> specific to this asset</param>
+        /// <param name="assetData">A byte array containing the raw asset data</param>
         public AssetTexture(UUID assetID, byte[] assetData) : base(assetID, assetData) { }
         
+        /// <summary>
+        /// Initializes a new instance of an AssetTexture object
+        /// </summary>
+        /// <param name="image">A <seealso cref="ManagedImage"/> object containing texture data</param>
         public AssetTexture(ManagedImage image)
         {
             Image = image;
@@ -320,41 +413,82 @@ namespace OpenMetaverse
         }
     }
 
+    /// <summary>
+    /// Represents a primitive asset
+    /// </summary>
     public class AssetPrim : Asset
     {
+
+        /// <summary>Override the base classes AssetType</summary>
         public override AssetType AssetType { get { return AssetType.Object; } }
 
+        /// <summary>Initializes a new instance of an AssetPrim object</summary>
         public AssetPrim() { }
 
+
+        /// <summary>
+        /// TODO: 
+        /// </summary>
         public override void Encode() { }
+
+        /// <summary>
+        /// TODO: 
+        /// </summary>
+        /// <returns>true</returns>
         public override bool Decode() { return true; }
     }
 
+    /// <summary>
+    /// Represents a Wearable Asset, Clothing, Hair, Skin, Etc
+    /// </summary>
     public abstract class AssetWearable : Asset
     {
+        /// <summary>A string containing the name of the asset</summary>
         public string Name = String.Empty;
+        /// <summary>A string containing a short description of the asset</summary>
         public string Description = String.Empty;
+        /// <summary>The Assets WearableType</summary>
         public WearableType WearableType = WearableType.Shape;
+        /// <summary>The For-Sale status of the object</summary>
         public SaleType ForSale;
+        /// <summary>An Integer representing the purchase price of the asset</summary>
         public int SalePrice;
+        /// <summary>The <seealso cref="UUID"/> of the assets creator</summary>
         public UUID Creator;
+        /// <summary>The <seealso cref="UUID"/> of the assets current owner</summary>
         public UUID Owner;
+        /// <summary>The <seealso cref="UUID"/> of the assets prior owner</summary>
         public UUID LastOwner;
+        /// <summary>The <seealso cref="UUID"/> of the Group this asset is set to</summary>
         public UUID Group;
+        /// <summary>True if the asset is owned by a <seealso cref="Group"/></summary>
         public bool GroupOwned;
+        /// <summary>The Permissions mask of the asset</summary>
         public Permissions Permissions;
+        /// <summary>A Dictionary containing Key/Value pairs of the objects parameters</summary>
         public Dictionary<int, float> Params = new Dictionary<int, float>();
+        /// <summary>A Dictionary containing Key/Value pairs where the Key is the textures Index and the Value is the Textures <seealso cref="UUID"/></summary>
         public Dictionary<AppearanceManager.TextureIndex, UUID> Textures = new Dictionary<AppearanceManager.TextureIndex, UUID>();
 
+        /// <summary>Initializes a new instance of an AssetWearable object</summary>
         public AssetWearable() { }
 
+        /// <summary>Initializes a new instance of an AssetWearable object with parameters</summary>
+        /// <param name="assetID">A unique <see cref="UUID"/> specific to this asset</param>
+        /// <param name="assetData">A byte array containing the raw asset data</param>
         public AssetWearable(UUID assetID, byte[] assetData) : base(assetID, assetData) { }
 
+        /// <summary>Initializes a new instance of an AssetWearable object with parameters</summary>
+        /// <param name="source">A string containing the asset parameters</param>
         public AssetWearable(string source)
         {
             AssetData = Utils.StringToBytes(source);
         }
 
+        /// <summary>
+        /// Decode an assets byte encoded data to a string
+        /// </summary>
+        /// <returns>true if the asset data was decoded successfully</returns>
         public override bool Decode()
         {
             int version = -1;
@@ -487,6 +621,9 @@ namespace OpenMetaverse
             return true;
         }
 
+        /// <summary>
+        /// Encode the assets string represantion into a format consumable by the asset server
+        /// </summary>
         public override void Encode()
         {
             const string NL = "\n";
@@ -528,21 +665,48 @@ namespace OpenMetaverse
         }
     }
 
+
+    /// <summary>
+    /// Represents an <seealso cref="AssetWearable"/> that can be worn on an avatar
+    /// such as a Shirt, Pants, etc.
+    /// </summary>
     public class AssetClothing : AssetWearable
     {
+        /// <summary>Override the base classes AssetType</summary>
         public override AssetType AssetType { get { return AssetType.Clothing; } }
 
+        /// <summary>Initializes a new instance of an AssetScriptBinary object</summary>
         public AssetClothing() { }
+
+        /// <summary>Initializes a new instance of an AssetScriptBinary object with parameters</summary>
+        /// <param name="assetID">A unique <see cref="UUID"/> specific to this asset</param>
+        /// <param name="assetData">A byte array containing the raw asset data</param>
         public AssetClothing(UUID assetID, byte[] assetData) : base(assetID, assetData) { }
+
+        /// <summary>Initializes a new instance of an AssetScriptBinary object with parameters</summary>
+        /// <param name="source">A string containing the Clothings data</param>
         public AssetClothing(string source) : base(source) { }
     }
 
+    /// <summary>
+    /// Represents an <seealso cref="AssetWearable"/> that represents an avatars body ie: Hair, Etc.
+    /// </summary>
     public class AssetBodypart : AssetWearable
     {
+        /// <summary>Override the base classes AssetType</summary>
         public override AssetType AssetType { get { return AssetType.Bodypart; } }
 
+        /// <summary>Initializes a new instance of an AssetBodyPart object</summary>
         public AssetBodypart() { }
+
+        /// <summary>Initializes a new instance of an AssetBodyPart object with parameters</summary>
+        /// <param name="assetID">A unique <see cref="UUID"/> specific to this asset</param>
+        /// <param name="assetData">A byte array containing the raw asset data</param>
         public AssetBodypart(UUID assetID, byte[] assetData) : base(assetID, assetData) { }
+
+        
+        /// <summary>Initializes a new instance of an AssetBodyPart object with parameters</summary>
+        /// <param name="source">A string representing the values of the Bodypart</param>
         public AssetBodypart(string source) : base(source) { }
     }
 }
