987f504e86356be1728b253a50ab775e8b9eef95
John Hurliman <jhurliman@jhurliman.org>
2009-03-26 02:18:43 +0000

    * Made Primitive.GetHashCode() much more thorough. Could reasonably be used as a prim CRC now
    git-svn-id: http://libopenmetaverse.googlecode.com/svn/libopenmetaverse/trunk@2540 52acb1d6-8a22-11de-b505-999d5b087335

--- OpenMetaverse/Primitives/Primitive.cs
+++ OpenMetaverse/Primitives/Primitive.cs
@@ -742,6 +742,17 @@ namespace OpenMetaverse
 
                 return flex;
             }
+
+            public override int GetHashCode()
+            {
+                return
+                    Softness.GetHashCode() ^
+                    Gravity.GetHashCode() ^
+                    Drag.GetHashCode() ^
+                    Wind.GetHashCode() ^
+                    Tension.GetHashCode() ^
+                    Force.GetHashCode();
+            }
         }
 
         /// <summary>
@@ -845,6 +856,16 @@ namespace OpenMetaverse
                 return light;
             }
 
+            public override int GetHashCode()
+            {
+                return
+                    Color.GetHashCode() ^
+                    Intensity.GetHashCode() ^
+                    Radius.GetHashCode() ^
+                    Cutoff.GetHashCode() ^
+                    Falloff.GetHashCode();
+            }
+
             /// <summary>
             /// 
             /// </summary>
@@ -923,6 +944,11 @@ namespace OpenMetaverse
                 }
 
                 return sculpt;
+            }
+
+            public override int GetHashCode()
+            {
+                return SculptTexture.GetHashCode() ^ Type.GetHashCode();
             }
         }
 
@@ -1508,7 +1534,51 @@ namespace OpenMetaverse
 
         public override int GetHashCode()
         {
-            return ID.GetHashCode();
+            return
+                Position.GetHashCode() ^
+                Velocity.GetHashCode() ^
+                Acceleration.GetHashCode() ^
+                Rotation.GetHashCode() ^
+                AngularVelocity.GetHashCode() ^
+                ClickAction.GetHashCode() ^
+                (Flexible != null ? Flexible.GetHashCode() : 0) ^
+                (Light != null ? Light.GetHashCode() : 0) ^
+                (Sculpt != null ? Sculpt.GetHashCode() : 0) ^
+                Flags.GetHashCode() ^
+                PrimData.Material.GetHashCode() ^
+                MediaURL.GetHashCode() ^
+                //TODO: NameValues?
+                Properties.OwnerID.GetHashCode() ^
+                ParentID.GetHashCode() ^
+                PrimData.PathBegin.GetHashCode() ^
+                PrimData.PathCurve.GetHashCode() ^
+                PrimData.PathEnd.GetHashCode() ^
+                PrimData.PathRadiusOffset.GetHashCode() ^
+                PrimData.PathRevolutions.GetHashCode() ^
+                PrimData.PathScaleX.GetHashCode() ^
+                PrimData.PathScaleY.GetHashCode() ^
+                PrimData.PathShearX.GetHashCode() ^
+                PrimData.PathShearY.GetHashCode() ^
+                PrimData.PathSkew.GetHashCode() ^
+                PrimData.PathTaperX.GetHashCode() ^
+                PrimData.PathTaperY.GetHashCode() ^
+                PrimData.PathTwist.GetHashCode() ^
+                PrimData.PathTwistBegin.GetHashCode() ^
+                PrimData.PCode.GetHashCode() ^
+                PrimData.ProfileBegin.GetHashCode() ^
+                PrimData.ProfileCurve.GetHashCode() ^
+                PrimData.ProfileEnd.GetHashCode() ^
+                PrimData.ProfileHollow.GetHashCode() ^
+                ParticleSys.GetHashCode() ^
+                TextColor.GetHashCode() ^
+                TextureAnim.GetHashCode() ^
+                (Textures != null ? Textures.GetHashCode() : 0) ^
+                SoundRadius.GetHashCode() ^
+                Scale.GetHashCode() ^
+                Sound.GetHashCode() ^
+                PrimData.State.GetHashCode() ^
+                Text.GetHashCode() ^
+                TreeSpecies.GetHashCode();
         }
 
         #endregion Overrides
