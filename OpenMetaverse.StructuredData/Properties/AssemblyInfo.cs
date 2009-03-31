b4d7063472f0bd25be460ed292442ac2699341d4
Jim Radford <jradford@npl.com>
2009-03-31 21:02:19 +0000

    Removed AssemblyInfo.cs from Types project to fix compilation errors on Visual Studio
    git-svn-id: http://libopenmetaverse.googlecode.com/svn/libopenmetaverse/branches/0.6-devel@2567 52acb1d6-8a22-11de-b505-999d5b087335

--- OpenMetaverse.StructuredData/Properties/AssemblyInfo.cs
+++ OpenMetaverse.StructuredData/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OpenMetaverse StructuredData")]
+[assembly: AssemblyTitle("OpenMetaverse.StructuredData")]
 [assembly: AssemblyDescription("LLSD and Json Library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("OpenMetaverse")]
