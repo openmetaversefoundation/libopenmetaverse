6bfe31a0b48ac884a6f41422f0ccbfaf61488e67
omegaworks <example@example.com>
2009-06-16 07:25:42 +0000

    Pfft, no need for apply :p
    git-svn-id: http://libopenmetaverse.googlecode.com/svn/libopenmetaverse/branches/modularized@2893 52acb1d6-8a22-11de-b505-999d5b087335

--- OpenMetaverse/Settings.cs
+++ OpenMetaverse/Settings.cs
@@ -51,13 +51,21 @@ namespace OpenMetaverse
         public static string RESOURCE_DIR = "openmetaverse_data";
 
         /// <summary>Login server to connect to</summary>
-        public string LOGIN_SERVER = AGNI_LOGIN_SERVER;
+        public string LOGIN_SERVER
+        {
+            get { return Client.Network.LoginServer; }
+            set { Client.Network.LoginServer = value; }
+        }
 
         /// <summary>IP Address the client will bind to</summary>
         public static System.Net.IPAddress BIND_ADDR = System.Net.IPAddress.Any;
 
         /// <summary>Use XML-RPC Login or LLSD Login, default is XML-RPC Login</summary>
-        public bool USE_LLSD_LOGIN = false;
+        public bool USE_LLSD_LOGIN
+        {
+            get { return Client.Network.UseLLSDLogin; }
+            set { Client.Network.UseLLSDLogin = value; }
+        }
         #endregion
         #region Inventory
         /// <summary>
@@ -79,30 +87,58 @@ namespace OpenMetaverse
 
         /// <summary>Number of milliseconds before a teleport attempt will time
         /// out</summary>
-        public int TELEPORT_TIMEOUT = 40 * 1000;
+        public int TELEPORT_TIMEOUT
+        {
+            get { return Client.Self.TeleportTimeout; }
+            set { Client.Self.TeleportTimeout = value; }
+        }
 
         /// <summary>Number of milliseconds before NetworkManager.Logout() will
         /// time out</summary>
-        public int LOGOUT_TIMEOUT = 5 * 1000;
+        public int LOGOUT_TIMEOUT
+        {
+            get { return Client.Network.LogoutTimeout; }
+            set { Client.Network.LogoutTimeout = value; }
+        }
 
         /// <summary>Number of milliseconds before a CAPS call will time out</summary>
         /// <remarks>Setting this too low will cause web requests time out and
         /// possibly retry repeatedly</remarks>
-        public int CAPS_TIMEOUT = 60 * 1000;
+        public int CAPS_TIMEOUT
+        {
+            get { return Client.Network.CapsTimeout; }
+            set { Client.Network.CapsTimeout = value; }
+        }
 
         /// <summary>Number of milliseconds for xml-rpc to timeout</summary>
-        public int LOGIN_TIMEOUT = 60 * 1000;
+        public int LOGIN_TIMEOUT
+        {
+            get { return Client.Network.LoginTimeout; }
+            set { Client.Network.LoginTimeout = value; }
+        }
 
         /// <summary>Milliseconds before a packet is assumed lost and resent</summary>
-        public int RESEND_TIMEOUT = 4000;
+        public int RESEND_TIMEOUT
+        {
+            get { return Client.Network.ResendTimeout; }
+            set { Client.Network.ResendTimeout = value; }
+        }
 
         /// <summary>Milliseconds without receiving a packet before the 
         /// connection to a simulator is assumed lost</summary>
-        public int SIMULATOR_TIMEOUT = 30 * 1000;
+        public int SIMULATOR_TIMEOUT
+        {
+            get { return Client.Network.SimulatorTimeout; }
+            set { Client.Network.SimulatorTimeout = value; }
+        }
 
         /// <summary>Milliseconds to wait for a simulator info request through
         /// the grid interface</summary>
-        public int MAP_REQUEST_TIMEOUT = 5 * 1000;
+        public int MAP_REQUEST_TIMEOUT
+        {
+            get { return Client.Grid.MapRequestTimeout; }
+            set { Client.Grid.MapRequestTimeout = value; }
+        }
 
         /// <summary>Number of milliseconds between sending pings to each sim</summary>
         public const int PING_INTERVAL = 2200;
@@ -134,9 +170,17 @@ namespace OpenMetaverse
         public const int PACKET_ARCHIVE_SIZE = 200;
         /// <summary>Maximum number of queued ACKs to be sent before SendAcks()
         /// is forced</summary>
-        public int MAX_PENDING_ACKS = 10;
+        public int MAX_PENDING_ACKS
+        {
+            get { return Client.Network.MaxPendingAcks; }
+            set { Client.Network.MaxPendingAcks = value; }
+        }
         /// <summary>Network stats queue length (seconds)</summary>
-        public int STATS_QUEUE_SIZE = 5;
+        public int STATS_QUEUE_SIZE
+        {
+            get { return Client.Network.StatsQueueSize; }
+            set { Client.Network.StatsQueueSize = value; }
+        }
 
         #endregion
         #region Configuration options (mostly booleans)
@@ -149,14 +193,26 @@ namespace OpenMetaverse
         /// overhead, but several calls currently block for a long time and
         /// would need to be rewritten as asynchronous code before this is
         /// feasible</remarks>
-        public bool SYNC_PACKETCALLBACKS = false;
+        public bool SYNC_PACKETCALLBACKS
+        {
+            get { return Client.Network.SyncPacketCallbacks; }
+            set { Client.Network.SyncPacketCallbacks = value; }
+        }
 
         /// <summary>Enable/disable storing terrain heightmaps in the 
         /// TerrainManager</summary>
-        public bool STORE_LAND_PATCHES = false;
+        public bool STORE_LAND_PATCHES
+        {
+            get { return Client.Terrain.StoreLandPatches; }
+            set { Client.Terrain.StoreLandPatches = value; }
+        }
 
         /// <summary>Enable/disable sending periodic camera updates</summary>
-        public bool SEND_AGENT_UPDATES = true;
+        public bool SEND_AGENT_UPDATES
+        {
+            get { return Client.Self.SendAgentUpdates; }
+            set { Client.Self.SendAgentUpdates = value; }
+        }
 
         /// <summary>Enable/disable automatically setting the bandwidth throttle
         /// after connecting to each simulator</summary>
@@ -164,79 +220,140 @@ namespace OpenMetaverse
         /// bandwidth setting in the official client. If you do not set a
         /// throttle your connection will by default be throttled well below
         /// the minimum values and you may experience connection problems</remarks>
-        public bool SEND_AGENT_THROTTLE = true;
+        public bool SEND_AGENT_THROTTLE
+        {
+            get { return Client.Throttle.SendAgentThrottle; }
+            set { Client.Throttle.SendAgentThrottle = value; }
+        }
 
         /// <summary>Enable/disable the sending of pings to monitor lag and 
         /// packet loss</summary>
-        public bool SEND_PINGS = true;
+        public bool SEND_PINGS
+        {
+            get { return Client.Network.SendPings; }
+            set { Client.Network.SendPings = value; }
+        }
 
         /// <summary>Should we connect to multiple sims? This will allow
         /// viewing in to neighboring simulators and sim crossings
         /// (Experimental)</summary>
-        public bool MULTIPLE_SIMS = true;
+        public bool MULTIPLE_SIMS
+        {
+            get { return Client.Network.MultipleSims; }
+            set { Client.Network.MultipleSims = value; }
+        }
 
         /// <summary>If true, all object update packets will be decoded in to
         /// native objects. If false, only updates for our own agent will be
         /// decoded. Registering an event handler will force objects for that
         /// type to always be decoded. If this is disabled the object tracking
         /// will have missing or partial prim and avatar information</summary>
-        public bool ALWAYS_DECODE_OBJECTS = true;
+        public bool ALWAYS_DECODE_OBJECTS
+        {
+            get { return Client.Objects.AlwaysDecodeObjects; }
+            set { Client.Objects.AlwaysDecodeObjects = value; }
+        }
 
         /// <summary>If true, when a cached object check is received from the
         /// server the full object info will automatically be requested</summary>
-        public bool ALWAYS_REQUEST_OBJECTS = true;
+        public bool ALWAYS_REQUEST_OBJECTS
+        {
+            get { return Client.Objects.AlwaysRequestObjects; }
+            set { Client.Objects.AlwaysRequestObjects = value; }
+        }
 
         /// <summary>Whether to establish connections to HTTP capabilities
         /// servers for simulators</summary>
-        public bool ENABLE_CAPS = true;
+        public bool ENABLE_CAPS
+        {
+            get { return Client.Network.EnableCaps; }
+            set { Client.Network.EnableCaps = value; }
+        }
 
         /// <summary>Whether to decode sim stats</summary>
-        public bool ENABLE_SIMSTATS = true;
+        public bool ENABLE_SIMSTATS
+        {
+            get { return Client.Network.EnableSimStats; }
+            set { Client.Network.EnableSimStats = value; }
+        }
 
         /// <summary>The capabilities servers are currently designed to
         /// periodically return a 502 error which signals for the client to
         /// re-establish a connection. Set this to true to log those 502 errors</summary>
+        /// <remarks>Not used.</remarks>
         public bool LOG_ALL_CAPS_ERRORS = false;
 
         /// <summary>If true, any reference received for a folder or item
         /// the library is not aware of will automatically be fetched</summary>
-        public bool FETCH_MISSING_INVENTORY = true;
+        public bool FETCH_MISSING_INVENTORY
+        {
+            get { return Client.Inventory.FetchMissingInventory; }
+            set { Client.Inventory.FetchMissingInventory = value; }
+        }
 
         /// <summary>If true, and <code>SEND_AGENT_UPDATES</code> is true,
         /// AgentUpdate packets will continuously be sent out to give the bot
         /// smoother movement and autopiloting</summary>
-        public bool DISABLE_AGENT_UPDATE_DUPLICATE_CHECK = true;
+        public bool DISABLE_AGENT_UPDATE_DUPLICATE_CHECK
+        {
+            get { return Client.Self.DisableAgentUpdateDuplicateCheck; }
+            set { Client.Self.DisableAgentUpdateDuplicateCheck = value; }
+        }
 
         /// <summary>If true, currently visible avatars will be stored
         /// in dictionaries inside <code>Simulator.ObjectAvatars</code>.
         /// If false, a new Avatar or Primitive object will be created
         /// each time an object update packet is received</summary>
-        public bool AVATAR_TRACKING = true;
+        public bool AVATAR_TRACKING
+        {
+            get { return Client.Objects.AvatarTracking; }
+            set
+            {
+                Client.Objects.AvatarTracking = value;
+                Client.Avatars.UpdateAvatarAppearance = value;
+            }
+        }
 
         /// <summary>If true, currently visible avatars will be stored
         /// in dictionaries inside <code>Simulator.ObjectPrimitives</code>.
         /// If false, a new Avatar or Primitive object will be created
         /// each time an object update packet is received</summary>
-        public bool OBJECT_TRACKING = true;
+        public bool OBJECT_TRACKING
+        {
+            get { return Client.Objects.ObjectTracking; }
+            set { Client.Objects.ObjectTracking = value; }
+        }
 
         #endregion
         #region Parcel Tracking
 
         /// <summary>If true, parcel details will be stored in the 
         /// <code>Simulator.Parcels</code> dictionary as they are received</summary>
-        public bool PARCEL_TRACKING = true;
+        public bool PARCEL_TRACKING
+        {
+            get { return Client.Parcels.ParcelTracking; }
+            set { Client.Parcels.ParcelTracking = value; }
+        }
 
         /// <summary>
         /// If true, an incoming parcel properties reply will automatically send
         /// a request for the parcel access list
         /// </summary>
-        public bool ALWAYS_REQUEST_PARCEL_ACL = true;
+        public bool ALWAYS_REQUEST_PARCEL_ACL
+        {
+            get { return Client.Parcels.AlwaysRequestParcelACL; }
+            set { Client.Parcels.AlwaysRequestParcelACL = value; }
+        }
 
         /// <summary>
         /// if true, an incoming parcel properties reply will automatically send 
         /// a request for the traffic count.
         /// </summary>
-        public bool ALWAYS_REQUEST_PARCEL_DWELL = true;
+        public bool ALWAYS_REQUEST_PARCEL_DWELL
+        {
+            get { return Client.Parcels.AlwaysRequestParcelDwell; }
+            set { Client.Parcels.AlwaysRequestParcelDwell = value; }
+        }
 
         #endregion
         #region Texture Cache
@@ -245,25 +362,49 @@ namespace OpenMetaverse
         /// If true, images downloaded from the server will be cached 
         /// in a local directory
         /// </summary>
-        public bool USE_TEXTURE_CACHE = false;
+        public bool USE_TEXTURE_CACHE
+        {
+            get { return Client.Assets.UseTextureCache; }
+            set { Client.Assets.UseTextureCache = value; }
+        }
 
         /// <summary>Path to store cached texture data</summary>
-        public string TEXTURE_CACHE_DIR = RESOURCE_DIR + "/cache";
+        public string TEXTURE_CACHE_DIR
+        {
+            get { return Client.Assets.TextureCacheDir; }
+            set { Client.Assets.TextureCacheDir = value; }
+        }
 
         /// <summary>Maximum size cached files are allowed to take on disk (bytes)</summary>
-        public long TEXTURE_CACHE_MAX_SIZE = 1024 * 1024 * 1024; // 1GB
+        public long TEXTURE_CACHE_MAX_SIZE
+        {
+            get { return Client.Assets.TextureCacheMaxSize; }
+            set { Client.Assets.TextureCacheMaxSize = value; }
+        }
 
         #endregion
         #region Misc
 
         /// <summary>Default color used for viewer particle effects</summary>
-        public Color4 DEFAULT_EFFECT_COLOR = new Color4(255, 0, 0, 255);
+        public Color4 DEFAULT_EFFECT_COLOR
+        {
+            get { return Client.Inventory.DefaultEffectColor; }
+            set { Client.Inventory.DefaultEffectColor = value; }
+        }
 
         /// <summary>Maximum number of times to resend a failed packet</summary>
-        public int MAX_RESEND_COUNT = 3;
+        public int MAX_RESEND_COUNT
+        {
+            get { return Client.Network.MaxResendCount; }
+            set { Client.Network.MaxResendCount = value; }
+        }
 
         /// <summary>Throttle outgoing packet rate</summary>
-        public bool THROTTLE_OUTGOING_PACKETS = true;
+        public bool THROTTLE_OUTGOING_PACKETS
+        {
+            get { return Client.Network.ThrottleOutgoingPackets; }
+            set { Client.Network.ThrottleOutgoingPackets = value; }
+        }
 
         #endregion
         #region Texture Pipeline
@@ -271,7 +412,11 @@ namespace OpenMetaverse
         /// <summary>The maximum number of concurrent texture downloads allowed</summary>
         /// <remarks>Increasing this number will not necessarily increase texture retrieval times due to
         /// simulator throttles</remarks>
-        public int MAX_CONCURRENT_TEXTURE_DOWNLOADS = 4;
+        public int MAX_CONCURRENT_TEXTURE_DOWNLOADS
+        {
+            get { return Client.Assets.MaxConcurrentTextureDownloads; }
+            set { Client.Assets.MaxConcurrentTextureDownloads = value; }
+        }
 
         /// <summary>
         /// The Refresh timer inteval is used to set the delay between checks for stalled texture downloads
@@ -282,7 +427,11 @@ namespace OpenMetaverse
         /// <summary>
         /// Textures taking longer than this value will be flagged as timed out and removed from the pipeline
         /// </summary>
-        public int PIPELINE_REQUEST_TIMEOUT = 45*1000;
+        public int PIPELINE_REQUEST_TIMEOUT
+        {
+            get { return Client.Assets.PipelineRequestTimeout; }
+            set { Client.Assets.PipelineRequestTimeout = value; }
+        }
         #endregion
 
         #region Logging Configuration
@@ -297,10 +446,18 @@ namespace OpenMetaverse
         public static Helpers.LogLevel LOG_LEVEL = Helpers.LogLevel.Debug;
 
         /// <summary>Attach avatar names to log messages</summary>
-        public bool LOG_NAMES = true;
+        public bool LOG_NAMES
+        {
+            get { return Client.Log.LogNames; }
+            set { Client.Log.LogNames = value; }
+        }
 
         /// <summary>Log packet retransmission info</summary>
-        public bool LOG_RESENDS = true;
+        public bool LOG_RESENDS
+        {
+            get { return Client.Network.LogResends; }
+            set { Client.Network.LogResends = value; }
+        }
 
         #endregion
 
@@ -309,50 +466,6 @@ namespace OpenMetaverse
         public Settings(GridClient client)
         {
             Client = client;
-            apply();
-        }
-        public void apply()
-        {
-            // Apply NetworkManager settings:
-            Client.Network.SimulatorTimeout = SIMULATOR_TIMEOUT;
-            Client.Network.LogoutTimeout = LOGOUT_TIMEOUT;
-            Client.Network.SyncPacketCallbacks = SYNC_PACKETCALLBACKS;
-            Client.Network.EnableSimStats = ENABLE_SIMSTATS;
-            Client.Network.MultipleSims = MULTIPLE_SIMS;
-            Client.Network.EnableCaps = ENABLE_CAPS;
-            Client.Network.ThrottleOutgoingPackets = THROTTLE_OUTGOING_PACKETS;
-            Client.Network.MaxPendingAcks = MAX_PENDING_ACKS;
-            Client.Network.ResendTimeout = RESEND_TIMEOUT;
-            Client.Network.MaxResendCount = MAX_RESEND_COUNT;
-            Client.Network.LogResends = LOG_RESENDS;
-            Client.Network.StatsQueueSize = STATS_QUEUE_SIZE;
-            Client.Network.SendPings = SEND_PINGS;
-            Client.Network.CapsTimeout = CAPS_TIMEOUT;
-            Client.Network.LoginTimeout = LOGIN_TIMEOUT;
-            Client.Network.LoginServer = LOGIN_SERVER;
-            Client.Network.UseLLSDLogin = USE_LLSD_LOGIN;
-
-            Client.Self.TeleportTimeout = TELEPORT_TIMEOUT;
-            Client.Self.SendAgentUpdates = SEND_AGENT_UPDATES;
-            Client.Self.DisableAgentUpdateDuplicateCheck = DISABLE_AGENT_UPDATE_DUPLICATE_CHECK;
-            Client.Grid.MapRequestTimeout = MAP_REQUEST_TIMEOUT;
-            Client.Terrain.StoreLandPatches = STORE_LAND_PATCHES;
-            Client.Objects.AvatarTracking = AVATAR_TRACKING;
-            Client.Objects.ObjectTracking = OBJECT_TRACKING;
-            Client.Objects.AlwaysDecodeObjects = ALWAYS_DECODE_OBJECTS;
-            Client.Objects.AlwaysRequestObjects = ALWAYS_REQUEST_OBJECTS;
-            Client.Parcels.ParcelTracking = PARCEL_TRACKING;
-            Client.Parcels.AlwaysRequestParcelACL = ALWAYS_REQUEST_PARCEL_ACL;
-            Client.Parcels.AlwaysRequestParcelDwell = ALWAYS_REQUEST_PARCEL_DWELL;
-            Client.Assets.UseTextureCache = USE_TEXTURE_CACHE;
-            Client.Assets.TextureCacheDir = TEXTURE_CACHE_DIR;
-            Client.Assets.TextureCacheMaxSize = TEXTURE_CACHE_MAX_SIZE;
-            Client.Assets.MaxConcurrentTextureDownloads = MAX_CONCURRENT_TEXTURE_DOWNLOADS;
-            Client.Assets.PipelineRequestTimeout = PIPELINE_REQUEST_TIMEOUT;
-            Client.Avatars.UpdateAvatarAppearance = AVATAR_TRACKING;
-            Client.Inventory.FetchMissingInventory = FETCH_MISSING_INVENTORY;
-            Client.Inventory.DefaultEffectColor = DEFAULT_EFFECT_COLOR;
-            Client.Throttle.SendAgentThrottle = SEND_AGENT_THROTTLE;
         }
     }
 }
