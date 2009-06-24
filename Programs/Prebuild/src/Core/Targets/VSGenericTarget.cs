dd4d781f4b0cd11972697ddbe7834a01f7aa8bef
John Hurliman <jhurliman@jhurliman.org>
2009-06-24 00:52:43 +0000

    Fix for Prebuild generating project files that are incompatible with VC# express
    git-svn-id: http://libopenmetaverse.googlecode.com/svn/libopenmetaverse/trunk@2908 52acb1d6-8a22-11de-b505-999d5b087335

--- Programs/Prebuild/src/Core/Targets/VSGenericTarget.cs
+++ Programs/Prebuild/src/Core/Targets/VSGenericTarget.cs
@@ -729,7 +729,8 @@ namespace Prebuild.Core.Targets
 
 		private void WriteSolutionFiles(SolutionNode solution, StreamWriter ss)
 		{
-			WriteProject(ss, "Folder", solution.Guid, "Solution Files", "Solution Files", solution.Files);
+            if (solution.Files != null && solution.Files.Count > 0)
+			    WriteProject(ss, "Folder", solution.Guid, "Solution Files", "Solution Files", solution.Files);
 		}
 
 		private void WriteEmbeddedSolution(StreamWriter writer, SolutionNode embeddedSolution)
