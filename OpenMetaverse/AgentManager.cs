6ac80a35fc4519ec981ec3f654c74433e0e7a4cd
Latif Khalifa <latifer@streamgrid.net>
2010-04-29 00:07:31 +0000

    LIBOMV-782 * Removed unused GroupChatLeft event. * Remove group chat session that we are leaving
    git-svn-id: http://libopenmetaverse.googlecode.com/svn/libopenmetaverse/trunk@3303 52acb1d6-8a22-11de-b505-999d5b087335

--- OpenMetaverse/AgentManager.cs
+++ OpenMetaverse/AgentManager.cs
@@ -991,29 +991,6 @@ namespace OpenMetaverse
         }
 
         /// <summary>The event subscribers. null if no subcribers</summary>
-        private EventHandler<GroupChatLeftEventArgs> m_GroupChatLeft;
-
-        /// <summary>Raises the GroupChatLeft event</summary>
-        /// <param name="e">A GroupChatLeftEventArgs object containing the
-        /// data returned from the data server</param>
-        protected virtual void OnGroupChatLeft(GroupChatLeftEventArgs e)
-        {
-            EventHandler<GroupChatLeftEventArgs> handler = m_GroupChatLeft;
-            if (handler != null)
-                handler(this, e);
-        }
-
-        /// <summary>Thread sync lock object</summary>
-        private readonly object m_GroupChatLeftLock = new object();
-
-        /// <summary>Raised when our agent exits a group chat session</summary>
-        public event EventHandler<GroupChatLeftEventArgs> GroupChatLeft
-        {
-            add { lock (m_GroupChatLeftLock) { m_GroupChatLeft += value; } }
-            remove { lock (m_GroupChatLeftLock) { m_GroupChatLeft -= value; } }
-        }
-
-        /// <summary>The event subscribers. null if no subcribers</summary>
         private EventHandler<AlertMessageEventArgs> m_AlertMessage;
 
         /// <summary>Raises the AlertMessage event</summary>
@@ -1665,6 +1642,10 @@ namespace OpenMetaverse
             im.MessageBlock.RegionID = UUID.Zero;
 
             Client.Network.SendPacket(im);
+
+            lock (GroupChatSessions.Dictionary)
+                if (GroupChatSessions.ContainsKey(groupID))
+                    GroupChatSessions.Remove(groupID);
         }
 
         /// <summary>
@@ -3851,11 +3832,6 @@ namespace OpenMetaverse
                         {
                             OnChatSessionMemberLeft(new ChatSessionMemberLeftEventArgs(msg.SessionID, msg.Updates[i].AgentID));
                         }
-
-                        if (msg.Updates[i].AgentID == Client.Self.AgentID)
-                        {
-                            OnGroupChatLeft(new GroupChatLeftEventArgs(msg.SessionID));
-                        }
                     }
                 }
 
@@ -4498,24 +4474,6 @@ namespace OpenMetaverse
             this.m_SessionName = sessionName;
             this.m_TmpSessionID = tmpSessionID;
             this.m_Success = success;
-        }
-    }
-
-    /// <summary>The session information when your agent exits a group chat session</summary>
-    public class GroupChatLeftEventArgs : EventArgs
-    {
-        private readonly UUID m_SessionID;
-
-        /// <summary>Get the ID of the session your agent left</summary>
-        public UUID SessionID { get { return m_SessionID; } }
-
-        /// <summary>
-        /// Construct a new instance of the GroupChatLeftEventArgs class
-        /// </summary>
-        /// <param name="chatSessionID">The ID of the session your agent left</param>
-        public GroupChatLeftEventArgs(UUID chatSessionID)
-        {
-            this.m_SessionID = chatSessionID;
         }
     }
 
