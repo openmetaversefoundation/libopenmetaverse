9a0ca2a9af81b9952d686216fb4e6edd3894e859
Latif Khalifa <latifer@streamgrid.net>
2010-05-02 13:17:08 +0000

    Update assembly version nuber for 0.8.0 release
    git-svn-id: http://libopenmetaverse.googlecode.com/svn/libopenmetaverse/branches/0.8-devel@3307 52acb1d6-8a22-11de-b505-999d5b087335

--- OpenMetaverse/AssemblyInfo.cs
+++ OpenMetaverse/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("openmetaverse.org")]
 [assembly: AssemblyProduct("OpenMetaverse")]
-[assembly: AssemblyCopyright("Copyright © openmetaverse.org 2006-2008")]
+[assembly: AssemblyCopyright("Copyright © openmetaverse.org 2006-2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]		
 
@@ -25,7 +25,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("0.0.0.*")]
+[assembly: AssemblyVersion("0.8.0.*")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
