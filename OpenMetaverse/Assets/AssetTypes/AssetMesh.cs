0837897926ad02501e74a9463b339ac9fb6b6842
Latif Khalifa <latifer@streamgrid.net>
2011-06-10 07:32:02 +0000

    Save all header data, not only subparts.
    git-svn-id: http://libopenmetaverse.googlecode.com/svn/libopenmetaverse/trunk@3545 52acb1d6-8a22-11de-b505-999d5b087335

--- OpenMetaverse/Assets/AssetTypes/AssetMesh.cs
+++ OpenMetaverse/Assets/AssetTypes/AssetMesh.cs
@@ -80,10 +80,17 @@ namespace OpenMetaverse.Assets
                     foreach(string partName in header.Keys)
                     {
                         if (header[partName].Type != OSDType.Map)
+                        {
+                            MeshData[partName] = header[partName];
                             continue;
+                        }
+
                         OSDMap partInfo = (OSDMap)header[partName];
                         if (partInfo["offset"] < 0 || partInfo["size"] == 0)
+                        {
+                            MeshData[partName] = partInfo;
                             continue;
+                        }
 
                         byte[] part = new byte[partInfo["size"]];
                         Buffer.BlockCopy(AssetData, partInfo["offset"] + (int)start, part, 0, part.Length);
