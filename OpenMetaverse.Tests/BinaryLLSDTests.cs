636eca9c07ab615c21d5a3ab9ff754e3f291f89b
Latif Khalifa <latifer@streamgrid.net>
2011-12-02 05:11:51 +0000

    Adjust tests to previous commit
    git-svn-id: http://libopenmetaverse.googlecode.com/svn/libopenmetaverse/trunk@3593 52acb1d6-8a22-11de-b505-999d5b087335

--- OpenMetaverse.Tests/BinaryLLSDTests.cs
+++ OpenMetaverse.Tests/BinaryLLSDTests.cs
@@ -49,7 +49,7 @@ namespace OpenMetaverse.Tests
     [TestFixture()]
     public class BinarySDTests
     {
-        private static readonly byte[] binaryHead = Encoding.ASCII.GetBytes("<? llsd/binary ?>");
+        private static readonly byte[] binaryHead = Encoding.ASCII.GetBytes("<?llsd/binary?>\n");
 
         [Test()]
         public void HelperFunctions()
