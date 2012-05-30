245f9ecade69a056239618044c22348ad751e19c
Douglas R. Miles <logicmoo@gmail.com>
2012-05-30 13:24:38 -0700

    LIBOMV - needed ResetControlFlags to be public for movement

--- OpenMetaverse/AgentManagerMovement.cs
+++ OpenMetaverse/AgentManagerMovement.cs
@@ -709,7 +709,7 @@ namespace OpenMetaverse
                 else agentControls &= ~((uint)flag);
             }
 
-            private void ResetControlFlags()
+            public void ResetControlFlags()
             {
                 // Reset all of the flags except for persistent settings like
                 // away, fly, mouselook, and crouching
