0ff6bee445cc0dcbed5a9aeb6573bfa94e8404ce
Latif Khalifa <latifer@streamgrid.net>
2012-07-13 08:10:36 +0200

    Consistent line endings

--- OpenMetaverse.Rendering.Meshmerizer/MeshmerizerR.cs
+++ OpenMetaverse.Rendering.Meshmerizer/MeshmerizerR.cs
@@ -1,4 +1,4 @@
-﻿/* Copyright (c) 2008 Robert Adams
+/* Copyright (c) 2008 Robert Adams
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *     * Redistributions of source code must retain the above copyright
