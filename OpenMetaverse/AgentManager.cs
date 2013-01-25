1ed090f4d25c44130c043124ad5ababdc5f8b7ba
Latif Khalifa <latifer@streamgrid.net>
2013-01-25 13:41:21 +0100

    Added new RequestLure type of IIM.
    See https://jira.secondlife.com/browse/storm-1838 for details

--- OpenMetaverse/AgentManager.cs
+++ OpenMetaverse/AgentManager.cs
@@ -142,9 +142,8 @@ namespace OpenMetaverse
         DenyTeleport = 24,
         /// <summary>Only useful if you have Linden permissions</summary>
         GodLikeRequestTeleport = 25,
-        /// <summary>A placeholder type for future expansion, currently not
-        /// used</summary>
-        CurrentlyUnused = 26,
+        /// <summary>Request a teleport lure</summary>
+        RequestLure = 26,
         // <summary>Notification of a new group election, this is 
         // deprecated</summary>
         //[Obsolete]
