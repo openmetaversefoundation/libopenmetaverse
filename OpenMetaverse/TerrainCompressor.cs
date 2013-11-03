0c54ae6f3cfbfbfb8dce11fb3aedbb0ee334450b
Robert Adams <Robert.Adams@intel.com>
2013-11-03 14:31:24 -0800

    Added extended terrain/cloud/wind/... compressed patch types for larger regions.

--- OpenMetaverse/TerrainCompressor.cs
+++ OpenMetaverse/TerrainCompressor.cs
@@ -36,10 +36,14 @@ namespace OpenMetaverse
 
         public enum LayerType : byte
         {
-            Land = 0x4C,
-            Water = 0x57,
-            Wind = 0x37,
-            Cloud = 0x38
+            Land            = 0x4C, // 'L'
+            LandExtended    = 0x4D, // 'M'
+            Water           = 0x57, // 'W'
+            WaterExtended   = 0x57, // 'X'
+            Wind            = 0x37, // '7'
+            WindExtended    = 0x39, // '9'
+            Cloud           = 0x38, // '8'
+            CloudExtended   = 0x3A  // ':'
         }
 
         public struct GroupHeader
