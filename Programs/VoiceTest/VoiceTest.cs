ae112d56fb7050d3f91b8bc8bca0eff794086fea
Latif Khalifa <latifer@streamgrid.net>
2014-02-25 15:31:10 +0100

    Update copyright notices

--- Programs/VoiceTest/VoiceTest.cs
+++ Programs/VoiceTest/VoiceTest.cs
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006-2008, openmetaverse.org
+ * Copyright (c) 2006-2014, openmetaverse.org
  * All rights reserved.
  *
  * - Redistribution and use in source and binary forms, with or without
